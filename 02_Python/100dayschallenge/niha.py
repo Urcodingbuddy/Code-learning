09faaa606ec9a5bd50dd87248ff02f066da340d2
Urcodingbuddy <anshpethe110@gmail.com>
2024-06-15 11:48:37 +0530

    Sheryians Practice js

---
+++ 02_Python/100dayschallenge/niha.py
@@ -0,0 +1,17 @@
+name = "helloe world" 
+ramukaka = 18 
+price = 25.9
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+print(ramukaka + nane)
+
+
+a = "45"
+b = "55"
+print(a+b)
+
+
+# write a pro
