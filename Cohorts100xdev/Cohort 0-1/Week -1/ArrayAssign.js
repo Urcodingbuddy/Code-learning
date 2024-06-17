a75c3e0cc46c595ed4de78502e6d7fa1f5880258
Urcodingbuddy <anshpethe110@gmail.com>
2024-06-18 00:19:40 +0530

    Basic Questions

--- Cohorts100xdev/Cohort 0-1/Week -1/ArrayAssign.js
+++ Cohorts100xdev/Cohort 0-1/Week -1/ArrayAssign.js
@@ -1,3 +1,42 @@
+// Q.1
+
+let Mfirstname = "Monish"
+
+let Mlastname = "Roy"
+
+let Ffirstname = "Moni"
+
+let Flastname = "Roy"
+
+
+console.log("Good afternoon : ", Mfirstname, Mlastname)
+console.log("Good afternoon : ", Ffirstname, Flastname)
+
+
+// Q.2
+
+let Mfullname = (Mfirstname+ Mlastname)
+let Ffullname = (Ffirstname+ Flastname)
+let Gender = "F"
+Gender.toUpperCase
+
+if(Gender==="M"){
+    console.log("Hello : Mr.",Mfullname)
+    return
+}
+else if(Gender==="F"){
+    console.log("Hello : Mrs.",Ffullname)
+}
+
+//Q.3
+
+for(i=1; i<=100;i++){
+    console.log(i)
+}
+
+/////////////////////////////////////////////////////////////
+
+
 const personarray = ["Anha","denver","julie","mark"]
 
 console.log(personarray[0])
