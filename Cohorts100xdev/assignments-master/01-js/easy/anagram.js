165630e0fbbca0f680d2ee1f6f311dc3fd717813
Urcodingbuddy <anshpethe110@gmail.com>
2024-06-18 23:02:44 +0530

    Week -01

--- Cohorts100xdev/assignments-master/01-js/easy/anagram.js
+++ Cohorts100xdev/assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
-
+    let lowstr1 = str1.toLowerCase();
+    let lowstr2 = str2.toLowerCase();
+    return lowstr1 === lowstr2;
 }
 
 module.exports = isAnagram;
+
+
+const isAnagram = require('./isAnagram');
+
+console.log(isAnagram('spar', 'rasp')); // true
+console.log(isAnagram('listen', 'silent')); // true
+console.log(isAnagram('hello', 'world')); // false
+console.log(isAnagram('Astronomer', 'Moon starer')); // true
