b9ae7fd7d3deb0eaa844306ab4de0b48f932ae01
Urcodingbuddy <anshpethe110@gmail.com>
2024-06-19 22:46:10 +0530

    Chalk deleted

---
+++ Youtube-Learning/02_Python/100dayschallenge/recurstion.py
@@ -0,0 +1,43 @@
+x = factorial(3)
+
+def factorial(n):
+
+if n == 1:
+
+return 1
+
+else:
+
+
+
+return n * factorial(n-1)
+
+ is
+
+2
+
+def factorial(n):
+
+if n == 1:
+
+2 is
+
+1
+
+return 1
+
+else:
+
+2
+
+return n * factorial(n-1)
+
+def factorial(n):
+
+if n == 1:
+
+return 1
+
+else:
+
+return n * factorial(n-1)
