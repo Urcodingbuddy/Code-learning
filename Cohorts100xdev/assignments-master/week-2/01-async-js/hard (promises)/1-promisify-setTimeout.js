664acd0a5789b81b6008052fb03a85f6037ce487
Urcodingbuddy <anshpethe110@gmail.com>
2024-06-22 20:59:22 +0530

    Reslove after n(param) seconds

--- Cohorts100xdev/assignments-master/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ Cohorts100xdev/assignments-master/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,19 @@
 */
 
 function wait(n) {
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Resolved");
+        },n*1000)
+    });
 }
 
-module.exports = wait;
+
+
+module.exports = wait(2);
+// console.log(wait(2))
+
+// Call the wait function and use the then method to print the resolved value
+wait(2).then((value) => {
+    console.log(value);
+});
