e01db905037da61878f7ed7de7ede4254707b0c0
Urcodingbuddy <anshpethe110@gmail.com>
2024-06-23 18:22:24 +0530

    Calculate Waiting Through 3 indivdual Promise

--- Cohorts100xdev/assignments-master/week-2/01-async-js/hard (promises)/3-promise-all.js
+++ Cohorts100xdev/assignments-master/week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,20 +4,41 @@
  * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
 
-function wait1(t) {
+function wait1(t){
+    return new Promise(resolve => {
+        setTimeout(resolve,t*1000)
+        console.log(resolve,"Wait1")
 
+})
 }
 
 function wait2(t) {
-
+    return new Promise(resolve => {
+        setTimeout(resolve,t*1000)
+        console.log(resolve,"Wait2")
+})
 }
 
 function wait3(t) {
+    return new Promise(resolve => {
+        setTimeout(resolve,t*1000)
+        console.log(resolve,"Wait3")
 
+})
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const start = Date.now();
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)])
+    .then(()=>{
+        const end = Date.now();
+        return end - start;
+    });
 }
 
+calculateTime(2,3,4).then(result=>{
+    console.log(result)
+})
+
 module.exports = calculateTime;
+
