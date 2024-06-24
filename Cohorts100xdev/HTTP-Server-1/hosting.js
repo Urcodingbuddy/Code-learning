ed3672997a88939d7598ab9d3917a1f2f0e74282
Urcodingbuddy <anshpethe110@gmail.com>
2024-06-24 19:53:04 +0530

    .

--- Cohorts100xdev/HTTP-Server-1/hosting.js
+++ Cohorts100xdev/HTTP-Server-1/hosting.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port =  process.env.POST || 3000;
 
 // Middleware to serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 console.log('Static files being served from:', path.join(__dirname, 'public'));
 
 // Route to serve the HTML file
-app.get('/', function(req, res) {
-    const filePath = path.join(__dirname, 'public', 'Bubble-game.html');
-    console.log('Serving file:', filePath);
-    res.sendFile(filePath);
+app.get('/bubble', function(req, res) {
+    const bubblePath = path.join(__dirname, 'public', 'Bubble-game.html');
+    console.log('Serving bubble game file:', bubblePath);
+    res.sendFile(bubblePath);
 });
+ 
+app.get('/responsive',function(req, res){
+    const responsive = path.join(__dirname,'public','FirstResponsive.html');
+    console.log('Serving resposive dummy site:', responsive);
+    res.sendFile(responsive)
+})
 
 // Start the server
 app.listen(port, function() {
