f60712a2e045551c196665c415c6fb968cde933f
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-09 23:48:37 +0530

    Server Spam Reqs blocker

--- Cohorts100xdev/assignments-master/week-3/01-middlewares/02-ratelimitter.js
+++ Cohorts100xdev/assignments-master/week-3/01-middlewares/02-ratelimitter.js
@@ -13,14 +13,33 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
-}); 
+function middletest(req, res, next) {
 
-app.post('/user', function(req, res) {
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] += 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("Too many Reqests, Please Try Again Leter :( ");
+    }
+    else{
+      next();
+    }
+  }
+  else{
+      next();
+      numberOfRequestsForUser[userId] = 1;
+  }
+
+}
+
+app.get('/user', function (req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
