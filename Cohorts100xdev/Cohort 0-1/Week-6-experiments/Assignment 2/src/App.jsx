1ab6f1f027c41930849c15fe417b65e9eb99a399
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-25 00:42:44 +0530

    Assingment 2 (get data by id )

--- Cohorts100xdev/Cohort 0-1/Week-6-experiments/Assignment 2/src/App.jsx
+++ Cohorts100xdev/Cohort 0-1/Week-6-experiments/Assignment 2/src/App.jsx
@@ -1,22 +1,25 @@
 
-
-/*
-
 import axios  from 'axios'
 import './App.css';
 import { useEffect, useState } from "react";
+
 function App() {
  return <div>
-  <Todo id={1}/>
+  <Todo id={5}/>
  </div>
 }
 function Todo({id}){
-  const [todo, setTodo] = useState([])
+  const [todo, setTodo] = useState({})
   useEffect(()=>{
-    axios.get(`https://sum-server.100xdevs.com/todo?id=1`)
-  })
+    axios.get(`https://sum-server.100xdevs.com/todo?id=`+id)
+    .then(async function(res){
+      const data = await res.data;
+      setTodo(data.todo)
+      console.log(data)
+    })
+  },[id])
     return(
-      <div style={{margin: '70px'}}>
+      <div  style={{margin: '70px'}}>
         <h1>
           {todo.title}
         </h1>
@@ -27,5 +30,3 @@ function Todo({id}){
     )
 }
 export default App
-
-*/ 
