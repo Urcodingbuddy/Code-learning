0e2f7f78b097bb1af1c8d44ccc6b8b75b6c38909
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-27 23:12:21 +0530

    useRef Done

--- Cohorts100xdev/Cohort 0-1/Week-6-Hooks/useRef/src/App.jsx
+++ Cohorts100xdev/Cohort 0-1/Week-6-Hooks/useRef/src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [incomeTax, setincomeTax] = useState(2000)
   const divRef =  useRef();
