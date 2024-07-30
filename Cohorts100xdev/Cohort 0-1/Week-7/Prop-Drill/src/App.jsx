84f24c055d6dd2e5f6fbaf7709a6c1ea6c063c1d
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-31 01:26:00 +0530

    Prop-drill chain system

--- Cohorts100xdev/Cohort 0-1/Week-7/Prop-Drill/src/App.jsx
+++ Cohorts100xdev/Cohort 0-1/Week-7/Prop-Drill/src/App.jsx
@@ -20,15 +20,8 @@ function Count({ count }) {
 
 function Buttons({count, setCount}) {
   return <div>
-    <button onClick={() => {
-      setCount(count-1)
-    }}> ➖ </button>
-
-    <button onClick={() => {
-      setCount(count+1)
-    }}> ➕ </button>
-
-
+    <button onClick={() => {setCount(count-1)}}> ➖ </button>
+    <button onClick={() => {setCount(count+1)}}> ➕ </button>
   </div>
 }
 
