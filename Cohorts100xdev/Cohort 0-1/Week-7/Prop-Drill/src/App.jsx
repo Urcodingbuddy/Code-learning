248c1829c8db9e6645941b88a831f27996b2892d
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-31 01:11:04 +0530

    Package Created with 3 Components

---
+++ Cohorts100xdev/Cohort 0-1/Week-7/Prop-Drill/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <Count count={count} />
+      <Buttons count={count} setCount={setCount} />
+    </div>
+  )
+}
+
+
+function Count({ count }) {
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({count, setCount}) {
+  return <div>
+    <button onClick={() => {
+      setCount(count-1)
+    }}> ➖ </button>
+
+    <button onClick={() => {
+      setCount(count+1)
+    }}> ➕ </button>
+
+
+  </div>
+}
+
+export default App
