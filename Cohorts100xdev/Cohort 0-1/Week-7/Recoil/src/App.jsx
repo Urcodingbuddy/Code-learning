7ff26f708ddbabfd08954963f8a1824daa1cbbe3
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-31 23:22:02 +0530

    State management using simple Recoild

--- Cohorts100xdev/Cohort 0-1/Week-7/Recoil/src/App.jsx
+++ Cohorts100xdev/Cohort 0-1/Week-7/Recoil/src/App.jsx
@@ -1,58 +1,53 @@
 import { useContext, useMemo, useState } from "react"
 import { CountContext } from "./context";
 import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { countAtom, evenSelector } from "./store/atoms/count";
+import { countAtom} from "./store/atom/count.jsx";
 
+import './App.css'
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div>
       <RecoilRoot>
-        <Count />
+        <Count/>
       </RecoilRoot>
     </div>
   )
 }
-
 function Count() {
-  console.log("re-render");
+  console.log("Count Re-renderd")
   return <div>
+    <RecoilRoot>
     <CountRenderer />
-    <Buttons />
+    <Buttons  />
+    </RecoilRoot>
   </div>
 }
+
+function Input(){
+  const [inputValue, setInputValue] = useState("")
+  return <div>
+    <input type="text" onChange={(e)=>{
+      setInputValue(e.target.value)
+    }} />
+  </div>
+}
+
 
 function CountRenderer() {
-  const count = useRecoilValue(countAtom);
-  
-  return <div>
-    <b>
-      {count}
-    </b>
-    <EvenCountRenderer />
-  </div>
+  const count = useRecoilValue(countAtom)
+  return <div>Count: {count}</div>
 }
 
-function EvenCountRenderer() {
-  const isEven = useRecoilValue(evenSelector);
-
-  return <div>
-    {isEven ? "It is even" : null}
-  </div>
-}
 
 function Buttons() {
-  const setCount = useSetRecoilState(countAtom);
-  console.log("buttons re-rendererd");
-
+  console.log("Button")
+  const setCount = useSetRecoilState(countAtom)
   return <div>
-    <button onClick={() => {
-      setCount(count => count + 1)
-    }}>Increase</button>
-
-    <button onClick={() => {
-      setCount(count => count - 1)
-    }}>Decrease</button>
+    <button onClick={() => { setCount(count => count - 1) }}> ➖ </button>
+    <button onClick={() => { setCount(count => count + 1) }}> ➕ </button>
   </div>
 }
-
 export default App
+
+
